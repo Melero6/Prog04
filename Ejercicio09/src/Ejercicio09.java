0c49d57a0d245b45d709b47fcf7e6a0af09376c7
Jorge Melero <xmelerikox@gmail.com>
2016-11-22 06:42:15 +0100

    a

--- Ejercicio09/src/Ejercicio09.java
+++ Ejercicio09/src/Ejercicio09.java
@@ -4,31 +4,69 @@ public class Ejercicio09 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado= new Scanner (System.in);
-		int errores=0;
+		boolean correcto=false;
+		int errores=0, hombresPrimero=0, mujeresPrimero=0, hombresSegundo=0, mujeresSegundo=0;
 		String entrada;
 		System.out.println("Introduce el codigo");
-		entrada = teclado.nextLine();
-	
-		
-		if (entrada.length()>8)
-			++errores;
+		entrada = teclado.next();
 
-		else if (entrada.substring(0,2)!="199")
+		while (correcto==false)
 		{
-			++errores;
+
+			if (entrada.length()>8)
+			{
+
+				System.out.println("ERROR: Codigo incorrecto");
+				++errores;
+			}
+			else if (entrada=="00000000")
+				correcto=true;
+			else if (entrada.substring(0,2)!="199")
+			{
+				System.out.println("ERROR: Codigo incorrecto");
+				++errores;
+			}
+			else if (entrada.charAt(3)!='0'&&entrada.charAt(3)!='1'&&entrada.charAt(3)!='2'&&entrada.charAt(3)!='4'&&entrada.charAt(3)!='5')
+			{
+				System.out.println("ERROR: Codigo incorrecto");
+				++errores;
+			}
+			else if (entrada.charAt(4)!='H'&&entrada.charAt(4)!='M')
+			{
+				System.out.println("ERROR: Codigo incorrecto");
+				++errores;
+			}
+			else if (entrada.charAt(5)!='1'&&entrada.charAt(5)!='2')
+			{
+				System.out.println("ERROR: Codigo incorrecto");
+				++errores;
+			}
+			else if (entrada.substring(4,5)=="H1")
+			{
+				++hombresPrimero;
+			}		
+			else if (entrada.substring(4,5)=="H2")
+			{
+				++hombresSegundo;
+			}		
+			else if (entrada.substring(4,5)=="M1")
+			{
+				++mujeresPrimero;
+			}		
+			else if (entrada.substring(4,5)=="M2")
+			{
+				++mujeresSegundo;
+			}
+			if(correcto==false)
+			{
+			System.out.println("Introduce el codigo");
+			entrada = teclado.next();
+			}
 		}
-		else if (entrada.charAt(3)!='0'&&entrada.charAt(3)!='1'&&entrada.charAt(3)!='2'&&entrada.charAt(3)!='4'&&entrada.charAt(3)!='5')
-		{
-			++errores;
-		}
-		else if (entrada.charAt(4)!='H'&&entrada.charAt(4)!='M')
-		{
-			++errores;
-		}
-		else if (entrada.charAt(5)!='1'&&entrada.charAt(5)!='2')
-		{
-			++errores;
-		}
+
+		System.out.println("Hay "+hombresPrimero+" hombres en primero y "+ hombresSegundo+" en segundo.");
+		System.out.println("Hay "+mujeresPrimero+" mujeres en primero y "+ mujeresSegundo+" en segundo.");
+		System.out.println("Se han cometido "+errores+" errores.");
 	}
 
 }
