f1db857fdd0b849662e97d3d2a182b0a10b54a30
Jorge.Melero <xmelerikox@gmail.com>
2016-11-22 21:15:39 +0100

    a

--- Ejercicio09/src/Ejercicio09.java
+++ Ejercicio09/src/Ejercicio09.java
@@ -4,64 +4,59 @@ public class Ejercicio09 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner teclado= new Scanner (System.in);
-		boolean correcto=false;
 		int errores=0, hombresPrimero=0, mujeresPrimero=0, hombresSegundo=0, mujeresSegundo=0;
-		String entrada;
+		String entrada, entradaSub45, entradaSub02;
 		System.out.println("Introduce el codigo");
 		entrada = teclado.next();
+		entradaSub45=entrada.substring(4,5);
+		System.out.println("Introduce el codigo");
+		entradaSub02=entrada.substring(0,2);
 
-		while (correcto==false)
+		while (!entrada.equals("00000000"))
 		{
 
-			if (entrada.length()>8)
+			if (entrada.length()!=8)
 			{
 
-				System.out.println("ERROR: Codigo incorrecto");
+				System.out.println("ERROR: Codigo incorrecto long");
 				++errores;
 			}
-			else if (entrada=="00000000")
-				correcto=true;
-			else if (entrada.substring(0,2)!="199")
+			else if (entradaSub02.equals("199"))
 			{
-				System.out.println("ERROR: Codigo incorrecto");
-				++errores;
+						
+				if (entrada.charAt(3)<'0'&&entrada.charAt(3)>'5')
+				{
+					System.out.println("ERROR: Codigo incorrecto 05");
+					++errores;
+				}
+				else if (entradaSub45.equalsIgnoreCase("H1"))
+					++hombresPrimero;
+						
+				else if (entradaSub45.equalsIgnoreCase("H2"))
+					++hombresSegundo;
+						
+				else if (entradaSub45.equalsIgnoreCase("M1"))
+					++mujeresPrimero;
+				
+				else if (entradaSub45.equalsIgnoreCase("M2"))
+					++mujeresSegundo;
+			
+				else
+				{
+					System.out.println("ERROR: Codigo incorrecto hm12");
+					++errores;
+				}
 			}
-			else if (entrada.charAt(3)!='0'&&entrada.charAt(3)!='1'&&entrada.charAt(3)!='2'&&entrada.charAt(3)!='4'&&entrada.charAt(3)!='5')
-			{
-				System.out.println("ERROR: Codigo incorrecto");
-				++errores;
-			}
-			else if (entrada.charAt(4)!='H'&&entrada.charAt(4)!='M')
-			{
-				System.out.println("ERROR: Codigo incorrecto");
-				++errores;
-			}
-			else if (entrada.charAt(5)!='1'&&entrada.charAt(5)!='2')
-			{
-				System.out.println("ERROR: Codigo incorrecto");
-				++errores;
-			}
-			else if (entrada.substring(4,5)=="H1")
-			{
-				++hombresPrimero;
-			}		
-			else if (entrada.substring(4,5)=="H2")
-			{
-				++hombresSegundo;
-			}		
-			else if (entrada.substring(4,5)=="M1")
-			{
-				++mujeresPrimero;
-			}		
-			else if (entrada.substring(4,5)=="M2")
-			{
-				++mujeresSegundo;
-			}
-			if(correcto==false)
-			{
+//			else
+//			{
+//				System.out.println("ERROR: Codigo incorrecto");
+//				++errores;
+//			}
+
 			System.out.println("Introduce el codigo");
 			entrada = teclado.next();
-			}
+			entradaSub45=entrada.substring(4,5);
+			entradaSub02=entrada.substring(0,2);
 		}
 
 		System.out.println("Hay "+hombresPrimero+" hombres en primero y "+ hombresSegundo+" en segundo.");
