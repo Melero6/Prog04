bd66e74e9102507f3e2d65f5de727b8a12b7b3fc
Jorge Melero <xmelerikox@gmail.com>
2016-11-24 08:57:57 +0100

    a

--- Ejercicio09/src/Ejercicio09.java
+++ Ejercicio09/src/Ejercicio09.java
@@ -5,58 +5,44 @@ public class Ejercicio09 {
 		// TODO Auto-generated method stub
 		Scanner teclado= new Scanner (System.in);
 		int errores=0, hombresPrimero=0, mujeresPrimero=0, hombresSegundo=0, mujeresSegundo=0;
-		String entrada, entradaSub45, entradaSub02;
+		String entrada;
+
 		System.out.println("Introduce el codigo");
 		entrada = teclado.next();
-		entradaSub45=entrada.substring(4,5);
-		System.out.println("Introduce el codigo");
-		entradaSub02=entrada.substring(0,2);
-
+		//199XXX00
+		//01234567
 		while (!entrada.equals("00000000"))
 		{
-
 			if (entrada.length()!=8)
-			{
-
-				System.out.println("ERROR: Codigo incorrecto long");
 				++errores;
-			}
-			else if (entradaSub02.equals("199"))
+			
+			else if (entrada.substring(0,3).equals("199"))
 			{
 						
-				if (entrada.charAt(3)<'0'&&entrada.charAt(3)>'5')
-				{
-					System.out.println("ERROR: Codigo incorrecto 05");
+				if (entrada.charAt(3)<'0'||entrada.charAt(3)>'5')
 					++errores;
-				}
-				else if (entradaSub45.equalsIgnoreCase("H1"))
+				
+				else if (entrada.substring(4,6).equalsIgnoreCase("H1"))
 					++hombresPrimero;
 						
-				else if (entradaSub45.equalsIgnoreCase("H2"))
+				else if (entrada.substring(4,6).equalsIgnoreCase("H2"))
 					++hombresSegundo;
 						
-				else if (entradaSub45.equalsIgnoreCase("M1"))
+				else if (entrada.substring(4,6).equalsIgnoreCase("M1"))
 					++mujeresPrimero;
 				
-				else if (entradaSub45.equalsIgnoreCase("M2"))
+				else if (entrada.substring(4,6).equalsIgnoreCase("M2"))
 					++mujeresSegundo;
 			
 				else
-				{
-					System.out.println("ERROR: Codigo incorrecto hm12");
 					++errores;
-				}
 			}
-//			else
-//			{
-//				System.out.println("ERROR: Codigo incorrecto");
-//				++errores;
-//			}
+			else
+				++errores;
 
 			System.out.println("Introduce el codigo");
 			entrada = teclado.next();
-			entradaSub45=entrada.substring(4,5);
-			entradaSub02=entrada.substring(0,2);
+
 		}
 
 		System.out.println("Hay "+hombresPrimero+" hombres en primero y "+ hombresSegundo+" en segundo.");
