14174cd7b85a3f58d9a7bb973279824eb8e25c58
Jorge.Melero <xmelerikox@gmail.com>
2016-11-24 19:24:50 +0100

    a

--- Ejercicio11/src/Ejercicio11.java
+++ Ejercicio11/src/Ejercicio11.java
@@ -5,17 +5,18 @@ public class Ejercicio11 {
 		// TODO Auto-generated method stub
 		Scanner teclado=new Scanner(System.in);
 		String entrada;
+		int f=0;
 		do
 		{
-			System.out.println("Introduce una entrada de 60 caracteres como maximo: ");
+			System.out.println("Introduce una entrada de 20 caracteres como maximo: ");
 			entrada= teclado.nextLine();
 		}
 		while(entrada.length()>20);
-		
-		for (int i=0;i<entrada.length();i++)
+		for (int i=entrada.length();i>0;i--)
 		{
-			//caracteres
-			
+			System.out.print(entrada.substring(entrada.length()-f,entrada.length()));
+			++f;
+			System.out.print(entrada.substring(0,i)+"\n");
 		}
 	}
 
